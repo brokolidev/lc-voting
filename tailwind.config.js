33bb78fa5644096d0474d980818c647bb402977b
brokoli <bocalist@gmail.com>
2021-06-15 00:52:36 +0900

    style for idea cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,7 @@ module.exports = {
                 'purple': '#8b60ed', 
             },
             spacing: {
+                44: '11rem',
                 70: '17.5rem',
                 175: '43.75rem',
             },
@@ -34,6 +35,13 @@ module.exports = {
             },
             fontFamily: {
                 sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                xxs: ['0.0625rem', { lineHeight: '1rem' }]
+            },
+            boxShadow: {
+                card: '4px 4px 15px 0 rgba(36, 37, 38, 0.08)',
+                dialog: '3px 4px 15px 0 rgba(36, 37, 38, 0.22)',
             },
         },
     },
@@ -44,5 +52,8 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
 };
