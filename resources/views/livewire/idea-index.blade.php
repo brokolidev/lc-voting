f12fbf21be33fa74b6353107133c22d5e06065d0
brokoli <bocalist@gmail.com>
2021-10-22 20:37:25 +0900

    did the current user vote vor the idea

--- resources/views/livewire/idea-index.blade.php
+++ resources/views/livewire/idea-index.blade.php
@@ -12,12 +12,16 @@
     class="idea-container bg-white rounded-xl flex hover:shadow-card transition duration-150 ease-in cursor-pointer">
     <div class="border-r border-gray-100 px-5 py-8 hidden md:block">
         <div class="text-center">
-            <div class="font-semibold text-2xl">{{ $votesCount }}</div>
+            <div class="font-semibold text-2xl @if ($hasVoted) text-blue @endif">{{ $votesCount }}</div>
             <div class="text-gray-500">Votes</div>
-        </div>
+    </div>
 
         <div class="mt-8">
-            <button class="w-20 bg-gray-200 border border-gray hover:border-gray-400 font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
+            @if ($hasVoted)
+                <button class="w-20 bg-blue border border-blue hover:bg-blue-hover text-white font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Voted</button>
+            @else
+                <button class="w-20 bg-gray-200 border border-gray hover:border-gray-400 font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
+            @endif
         </div>
     </div>
     <div class="flex px-2 py-6 flex-1 flex-col md:flex-row">
@@ -69,10 +73,14 @@
 
             <div class="mt-4 md:mt-0 md:hidden flex items-center">
                 <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
-                    <div class="text-sm font-bold reading-none">{{ $votesCount }}</div>
+                    <div class="text-sm font-bold reading-none @if ($hasVoted) text-blue @endif"">{{ $votesCount }}</div>
                     <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
                 </div>
-                <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
+                @if ($hasVoted)
+                    <button class="w-20 bg-blue border border-blue text-white font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5">Voted</button>
+                @else
+                    <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
+                @endif
             </div>
         </div>
     </div>
--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -50,10 +50,15 @@
 
                     <div class="mt-4 md:mt-0 md:hidden flex items-center">
                         <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
-                            <div class="text-sm font-bold reading-none">{{ $votesCount }}</div>
+                            <div class="text-sm font-bold reading-none @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                             <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
                         </div>
-                        <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
+                        @if ($hasVoted)
+                            <button class="w-20 bg-blue border text-white border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5">Voted</button>
+                        @else
+                            <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">Vote</button>
+                        @endif
+                        
                     </div>
                 </div>
             </div>
@@ -178,13 +183,18 @@
 
         <div class="md:flex items-center space-x-3 hidden">
             <div class="bg-white font-semibold text-center rounded-xl px-3 py-2">
-                <div class="text-xl leading-snug">{{ $votesCount }}</div>
+                <div class="text-xl leading-snug @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                 <div class="text-gray-400 text-xs leading-none">Votes</div>
             </div>
-
-            <button type="button" class="h-11 w-32 uppercase text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
-                <span>Vote</span>
-            </button>
+            @if ($hasVoted)
+                <button type="button" class="h-11 w-32 uppercase text-xs bg-blue text-white font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3">
+                    <span>Voted</span>
+                </button>
+            @else
+                <button type="button" class="h-11 w-32 uppercase text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3">
+                    <span>Vote</span>
+                </button>    
+            @endif
         </div>
     </div> <!-- end buttons container -->
 </div>
