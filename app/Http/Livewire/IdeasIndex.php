c969b8c55c50b0915b019f09b7160cc16f179de7
brokoli <bocalist@gmail.com>
2021-10-28 23:39:09 +0900

    added other filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -15,10 +15,12 @@ class IdeasIndex extends Component
 
     public $status;
     public $category;
+    public $filter;
 
     protected $queryString = [
         'status',
         'category',
+        'filter',
     ];
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -31,6 +33,15 @@ class IdeasIndex extends Component
     public function updatingCategory()
     {
         $this->resetPage();
+    }
+
+    public function updatingFilter()
+    {
+        if($this->filter === 'My Ideas') {
+            if(! auth()->check()) {
+                return redirect()->route('login');
+            }
+        }
     }
 
     public function queryStringUpdatedStatus($newStatus)
@@ -51,6 +62,12 @@ class IdeasIndex extends Component
                     })
                     ->when($this->category and $this->category !== 'All', function($query) use($categories) {
                         return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
+                    })
+                    ->when($this->filter and $this->filter === 'Top Voted', function($query) {
+                        return $query->orderByDesc('votes_count');
+                    })
+                    ->when($this->filter and $this->filter === 'My Ideas', function($query) {
+                        return $query->where('user_id', auth()->id());
                     })
                     ->addSelect(['voted_by_user' => Vote::select('id')
                         ->where('user_id', auth()->id())
--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -9,12 +9,10 @@
             </select>
         </div>
         <div class="w-full md:w-1/3">
-            <select name="other_filters" id="other_filters" class="w-full rounded-xl px-4 py-2 border-none">
-                <option value="Filters One">Filters One</option>
-                <option value="Filters Two">Filters Two</option>
-                <option value="Filters Three">Filters Three</option>
-                <option value="Filters Four">Filters Four</option>
-                <option value="Filters Five">Filters Five</option>
+            <select wire:model="filter" name="other_filters" id="other_filters" class="w-full rounded-xl px-4 py-2 border-none">
+                <option value="No Filter">No Filter</option>
+                <option value="Top Voted">Top Voted</option>
+                <option value="My Ideas">My Ideas</option>
             </select>
         </div>
         <div class="w-full md:w-2/3 relative">
