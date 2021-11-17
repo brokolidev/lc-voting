23cf7fb1e1d5c67c8231a520e9b4380c3db638bf
brokoli <bocalist@gmail.com>
2021-11-17 21:48:55 +0900

    Notification styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,14 +28,92 @@
             @if (Route::has('login'))
                 <div class="px-6 py-4">
                     @auth
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
+                        <div class="flex items-center space-x-4">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
 
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); this.closest('form').submit();">
-                                {{ __('Log out') }}
-                            </a>
-                        </form>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); this.closest('form').submit();">
+                                    {{ __('Log out') }}
+                                </a>
+                            </form>
+
+                            <div x-data="{ isOpen: false }" class="relative">
+                                <button 
+                                    @click="isOpen = !isOpen">
+                                <svg class="h-8 w-8 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                                    </svg>
+                                    <div class="absolute rounded-full bg-red text-white text-xxs w-5 h-5 flex justify-center items-center border-2 -top-1 -right-1">8</div>
+                                </button>
+                                <ul x-show.transition.origin.top.left="isOpen" @click.away="isOpen = false"
+                                    @keydown.escape.window="isOpen = false"
+                                    class="absolute w-76 md:w-96 bg-white shadow-dialog rounded-xl text-sm text-left z-10 max-h-128 overflow-y-auto text-gray-700 md:-right-8 -right-28">
+                                    <li>
+                                        <a href="#" @click.prevent="
+                                                    isOpen = false
+                                                        $dispatch('custom-show-edit-modal') 
+                                                    "
+                                        class="hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in flex">
+                                            <img src="https://www.gravatar.com/avatar/000000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+                                            <div class="ml-4">
+                                                <div class="line-clamp-6">
+                                                    <span class="font-semibold">최현승</span> commented on
+                                                    <span class="font-semibold">This is my idea</span>
+                                                    <span>"Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                                        Aspernatur delectus nesciunt ullam saepe obcaecati pariatur illum, 
+                                                        suscipit maiores iste! Repellendus hic aut temporibus laborum alias aliquid vero nisi adipisci eaque. 
+                                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                                        Aspernatur delectus nesciunt ullam saepe obcaecati pariatur illum, 
+                                                        suscipit maiores iste! Repellendus hic aut temporibus laborum alias aliquid vero nisi adipisci eaque."</span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#" @click.prevent="
+                                                    isOpen = false
+                                                        $dispatch('custom-show-edit-modal') 
+                                                    "
+                                        class="hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in flex">
+                                            <img src="https://www.gravatar.com/avatar/000000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+                                            <div class="ml-4">
+                                                <div>
+                                                    <span class="font-semibold">최현승</span> commented on
+                                                    <span class="font-semibold">This is my idea</span>
+                                                    <span>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus nesciunt ullam saepe obcaecati pariatur illum, suscipit maiores iste! Repellendus hic aut temporibus laborum alias aliquid vero nisi adipisci eaque."</span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#" @click.prevent="
+                                                    isOpen = false
+                                                        $dispatch('custom-show-edit-modal') 
+                                                    "
+                                        class="hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in flex">
+                                            <img src="https://www.gravatar.com/avatar/000000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
+                                            <div class="ml-4">
+                                                <div>
+                                                    <span class="font-semibold">최현승</span> commented on
+                                                    <span class="font-semibold">This is my idea</span>
+                                                    <span>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur delectus nesciunt ullam saepe obcaecati pariatur illum, suscipit maiores iste! Repellendus hic aut temporibus laborum alias aliquid vero nisi adipisci eaque."</span>
+                                                </div>
+                                                <div class="text-xs text-gray-500 mt-2">1 hour ago</div>
+                                            </div>
+                                        </a>
+                                    </li>
+
+                                    <li class="border-t border-gray-300 text-center">
+                                        <button class="w-full hover:bg-gray-100 px-5 py-4 transition duration-150 ease-in block font-semibold">Mark all as read</button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
