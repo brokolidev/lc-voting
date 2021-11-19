2a73e92130dd748b8293b08f514bedff9b067382
brokoli <bocalist@gmail.com>
2021-11-19 11:20:31 +0900

    notification badge and skeleton loader

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -7,17 +7,40 @@ use Livewire\Component;
 class CommentNotifications extends Component
 {
 
+    const NOTIFICATION_TRESHOLD = 10;
+
     public $notifications;
+    public $notificationCount;
+    public $isLoading;
+
+
     protected $listeners = ['getNotifications'];
 
     public function mount()
     {
         $this->notifications = collect([]);
+        $this->isLoading = true;
+        $this->getNofificationCount();
+    }
+
+    public function getNofificationCount()
+    {
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+
+        if($this->notificationCount > self::NOTIFICATION_TRESHOLD) {
+            $this->notificationCount = self::NOTIFICATION_TRESHOLD.'+';
+        }
     }
 
     public function getNotifications()
     {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = auth()->user()
+            ->unreadNotifications()
+            ->latest()
+            ->take(self::NOTIFICATION_TRESHOLD)
+            ->get();
+
+        $this->isLoading = false;
     }
 
     public function render()
